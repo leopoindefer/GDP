e4674341e11c2ba737ecdb066bb392c2dd4e4020
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-05 15:41:31 +0100

    Add files via upload

---
+++ IA_GDP.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle as pkle  
+import math
+import pandas as pd
+from prophet import Prophet
+import yfinance as yf
+
+st.set_page_config(
+    page_title="GDP",
+    page_icon="💯",
+)
+
+st.title("Gérer votre portefeuille avec l'IA")
+
+hide_st_style = """
+            <style>
+            #MainMenu {Visibility: hidden;}
+            footer {visibility: hidden;}
+            .bouton {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True) 
+
+st.header("Action à visualier")
+action = st.selectbox('Choisir une action', ('AAPL','META','AMZN','TSLA'))
+
+symb = action
+column = f"Close_{symb}"
+file = f"bdd/actions/{symb}.csv"
+df = pd.read_csv(file)
+df = df.rename(columns = {column:'y',"Date":"ds"})
+df=df.loc[:,["ds","y"]]
+
+m = Prophet()
+m.fit(df)
+future = m.make_future_dataframe(periods=365)
+forecast = m.predict(future)
+fig1 = m.plot(forecast)
+fig2 = m.plot_components(forecast)
