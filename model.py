b8ccf64e0b4ec3d0f1f5faac51cba915a66ef265
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-05 17:26:41 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from prophet import Prophet
+import yfinance as yf
+
+def model(df):
+    m = Prophet()
+    m.fit(df)
+    future = m.make_future_dataframe(periods=365)
+    forecast = m.predict(future)
+    return model
