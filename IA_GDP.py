589759864cf131ab5e28b1d5257bd4c2ae686e47
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-06 11:41:52 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -47,10 +47,11 @@ with tab1 :
             ecart = df.set_index('ds').join(predict.set_index('ds'), how="left")
             ecart['se'] = ecart['y'] - ecart['yhat']
             sec = ecart.sum(axis=0)
+            sec = sum(sec["se"])
 
             predict = predict.rename(columns={"ds":"date","yhat":"prediction"})
-
-            st.dataframe(sec)
+            
+            st.write(sec)
             st.line_chart(data=predict, x="date", y="prediction")
             st.dataframe(predict)
 
