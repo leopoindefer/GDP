1375d970394c39e601e0fe7f485ea835a2086d14
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-06 11:53:21 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -47,7 +47,7 @@ with tab1 :
             ecart = df.set_index('ds').join(predict.set_index('ds'), how="left")
             ecart['se'] = ecart['y'] - ecart['yhat']
             sec = ecart.sum(axis=0)
-            sec = ecart.iloc[-1].tolist()
+            sec = ecart.tolist()
 
             predict = predict.rename(columns={"ds":"date","yhat":"prediction"})
 
