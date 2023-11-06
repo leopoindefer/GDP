6a0d919e9f100c197436480bcb85f26c8ee6d9bf
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-06 12:43:33 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -63,10 +63,10 @@ with tab1 :
             with col1:
 
                 montant = st.text_input('Montant à investir', 1000)
-                start_value = df["y"].iloc[-1]
-                end_value = predict["prediction"].iloc[-1]
-                gap_indiv_value = (end_value - start_value)
-                invest_part = montant/start_value
+                #start_value = df["y"].iloc[-1]
+                #end_value = predict["prediction"].iloc[-1]
+                #gap_indiv_value = (end_value - start_value)
+                #invest_part = montant/start_value
 
                 ticker = yf.Ticker(symb)
                 div_indiv = pd.DataFrame(ticker.dividends)
