644bd78c7b073406279b0755545bddf41c920c70
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-07 10:59:33 +0100

    Add files via upload

--- fonctions/cleaner.py
+++ fonctions/cleaner.py
@@ -5,4 +5,5 @@ from datetime import date
 def df_prophet(df):
     df_prophet = df.rename(columns = {column:'y',"Date":"ds"})
     df_prophet = df_prophet.loc[:,["ds","y"]]
+    df_prophet['ds'] = pd.to_datetime(df_prophet['ds'])
     return df_prophet
