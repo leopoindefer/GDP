081140746becc8cdd4f83f9003140e468753da1a
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-07 11:06:19 +0100

    Delete fonctions/cleaner.py

--- fonctions/cleaner.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import datetime
-from datetime import date
-
-def df_prophet(df):
-    df_prophet = df.rename(columns = {column:'y',"Date":"ds"})
-    df_prophet = df_prophet.loc[:,["ds","y"]]
-    df_prophet['ds'] = pd.to_datetime(df_prophet['ds'])
-    return df_prophet
