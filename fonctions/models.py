6a28abe8f54aecf4a6f898c526435a29c245f2b4
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-07 14:05:52 +0100

    Add files via upload

--- fonctions/models.py
+++ fonctions/models.py
@@ -22,22 +22,12 @@ def arima_model(df):
     adf_shifted = adfuller(df_shifted)
 
     # Définir les différentes valeurs de p, d et q à tester
-    p_values = range(8, 13)
-    d_values = range(0, 5)
-    q_values = range(8, 13)
 
     # Stocker les résultats de l'AIC pour chaque combinaison de paramètres
     aic_results = []
 
     # Boucler sur toutes les combinaisons possibles de p, d et q
-    for p in p_values:
-        for d in d_values:
-            for q in q_values:
-                try:
-                    model = sm.tsa.ARIMA(df_shifted, order=(p,d,q)).fit()
+    model = sm.tsa.ARIMA(df_shifted, order=(1,0,0)).fit()
                     # Stocker l'AIC pour ce modèle
-                    aic_results.append((p,d,q,model.aic))
-                except: 
-                    continue
-
-   return sorted(aic_results, key=lambda x: x[3])[0]
+    
+   return model.summary()
