0e868eba3df78f47324c864a854219140a63a17b
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-07 14:49:07 +0100

    Add files via upload

--- fonctions/models.py
+++ fonctions/models.py
@@ -24,7 +24,7 @@ def arima_model(df):
     # Stocker les résultats de l'AIC pour chaque combinaison de paramètres
 
     # Boucler sur toutes les combinaisons possibles de p, d et q
-    model = sm.tsa.ARIMA(df_shifted, order=(1,0,0)).fit()
+    model = sm.tsa.ARIMA(df_shifted, order=(5,1,0)).fit()
     # Stocker l'AIC pour ce modèle
     arima_pred = model.forecast()
     return arima_pred
