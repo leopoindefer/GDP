577c428373a60eee71adb5c64ba44d3066c2629c
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-09 14:32:49 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -118,12 +118,12 @@ with tab1 :
             tRent = (div+gap_value)/montant*100
             nb_part = f'<span style="color: #FF0000;">{round(invest_part,2)}</span>'
             st.write(f'Nombre d action acheté : {nb_part}', unsafe_allow_html=True)
-            tx_rendement = f'<span style="color: #FF0000;">{round(tRend,2)}</span>'
-            rendement = f'<span style="color: #FF0000;">{round(div,2)}</span>'
-            st.write(f'Taux de rendement de : {tx_rendement}%, Rendement de {rendement}€', unsafe_allow_html=True)
-            tx_rentabilite = f'<span style="color: #FF0000;">{round(tRent,2)}</span>' 
-            rentabilite = f'<span style="color: #FF0000;">{round(div + gap_value,2)}</span>'
-            st.write(f'Taux de rentabilité de : {tx_rentabilite}%, Rentabilité de {rentabilite}€', unsafe_allow_html=True)
+            tx_rendement = f'<span style="color: #FF0000;">{round(tRend,2)}%</span>'
+            rendement = f'<span style="color: #FF0000;">{round(div,2)}€</span>'
+            st.write(f'Taux de rendement de : {tx_rendement}, Rendement de {rendement}', unsafe_allow_html=True)
+            tx_rentabilite = f'<span style="color: #FF0000;">{round(tRent,2)}%</span>' 
+            rentabilite = f'<span style="color: #FF0000;">{round(div + gap_value,2)}€</span>'
+            st.write(f'Taux de rentabilité de : {tx_rentabilite}, Rentabilité de {rentabilite}', unsafe_allow_html=True)
         except:
             st.error('pas de résultat')
     
