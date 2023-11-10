163e0b66fe2c39fab19c03de152373d7195b084a
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-10 10:19:24 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -198,7 +198,8 @@ with tab1 :
             liste_cours.append({"SYMBOLE": s_txt, "DERNIER": cours, "M-1": cours_prec, "VAR": f'{var}%'})
     macro = pd.DataFrame(liste_cours)
     
-    st.dataframe(macro.style.applymap(lambda x: 'color: green'if any('-' in words for words in x.split()) else 'color: red',subset = ['VAR']))
-
+    st.dataframe(macro.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']))
+    ticker_symbol = yf.Ticker("AAPL")
+    st.write(ticker_symbol)
 with tab2 : 
     st.header("Créer votre portefeuille")
