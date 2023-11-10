82c34846fc91b792f835617e124301c869540d73
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-10 21:26:48 +0100

    Add files via upload

--- fonctions/prophet.py
+++ fonctions/prophet.py
@@ -2,13 +2,10 @@ import pandas as pd
 import numpy as np
 from prophet import Prophet
 
-def prophet_model(df, symb):
-    column = f"Close_{symb}"
-    df = df.rename(columns = {column:'y',"Date":"ds"})
-    df = df.loc[:,["ds","y"]]
+def prophet_model(df):
     m = Prophet()
     m.fit(df)
     future = m.make_future_dataframe(periods=365)
     forecast = m.predict(future)
 
-    return df, forecast
+    return forecast
