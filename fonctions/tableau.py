ca1fa4aced7d17f14d5137c68c5a9e4c9deb9718
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-10 16:30:38 +0100

    Add files via upload

--- fonctions/tableau.py
+++ fonctions/tableau.py
@@ -18,7 +18,7 @@ def Tableau(symbol_txt,symbol):
             variation = s_cinq_ans[close_columns].pct_change().dropna()
             renta_moy = variation.values.mean()
             renta_moy = round(renta_moy*100,2)
-            risque_moy = np.std(s_cinq_ans[close_columns]).values
+            risque_moy = np.std(s_cinq_ans[close_columns]).values.mean()
             risque_moy = round(risque_moy,2)
             line = [str(val) for val in s_resampled[close_columns].values.flatten()]
             liste_cours.append({"SYMBOLE": s_txt, "DERNIER": cours, "M-1": cours_prec, "VAR": f'{var}%', "RENTABILITE": f'{renta_moy}%', "RISQUE": risque_moy, "VISION":line})
