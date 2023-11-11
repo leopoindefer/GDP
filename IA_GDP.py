29675169f03c9d08ff2898956071d10e67a2f66c
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-11 20:14:58 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -138,6 +138,7 @@ with tab3 :
 
     # Fusionnez les DataFrames en utilisant pd.concat
     merged_df = pd.concat(selected_dataframes, axis=1, join='inner')
+    merged_df.index = pd.to_datetime(merged_df.index)
     merged_df_mois = merged_df.resample('MS').first()
 
     # Affichez le DataFrame fusionné
