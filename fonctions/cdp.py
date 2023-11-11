a5c09fa60df8d1de1ed831d2713a0c515f944b71
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-11 19:06:03 +0100

    Add files via upload

--- fonctions/cdp.py
+++ fonctions/cdp.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def CDP(symbol, symbol_txt): 
-    for acts, acts_txt in zip(symbol, symbol_txt):
-        close_columns = [col for col in acts.columns if 'Close' in col]
-        acts = acts.set_index("Date")
+def CDP(dataframes):
+    df_ptf = dataframes[0]
+    for df in dataframes[1:]:
+        close_columns = [col for col in df.columns if 'Close' in col]
         if close_columns:
-            acts = acts.loc[:,close_columns]
-            df_ptf = pd.merge(acts, how="inner")
-            pd.DataFrame(df_ptf, inplace=True)
+            df = df.set_index("Date").loc[:, close_columns]
+            df_ptf = pd.merge(df_ptf, df, how="inner", left_index=True, right_index=True)
+
     return df_ptf
