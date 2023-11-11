bdbb9c0b3f8a1d7c0f6ee7dbb4a6c5c8411b2ead
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-11 18:25:22 +0100

    Add files via upload

--- fonctions/comparaison.py
+++ fonctions/comparaison.py
@@ -6,21 +6,17 @@ def Comparaison(symb1, symb2):
     column1 = f"Close_{symb1}"
     file1 = f"data/actions/{symb1}.csv"
     df1 = pd.read_csv(file1, parse_dates=['Date'])
-    
+
     column2 = f"Close_{symb2}"
     file2 = f"data/actions/{symb2}.csv"
     df2 = pd.read_csv(file2, parse_dates=['Date'])
-    
-    # Merge les deux dataframes sur la colonne des dates
+
     graph_comp = pd.merge(df1, df2, on='Date', how='inner')
     graph_comp = graph_comp.loc[:,["Date",column1,column2]]
     graph_comp.set_index("Date", inplace=True)
-    
-    # Utilisez les colonnes fusionnées pour calculer la corrélation
+
     X = graph_comp[column1].tolist()
     Y = graph_comp[column2].tolist()
-    
-    # Calcul de la corrélation de Pearson
     corr, _ = pearsonr(X, Y)
     
-    return column1, column2, graph_comp, corr
+    return graph_comp, corr
