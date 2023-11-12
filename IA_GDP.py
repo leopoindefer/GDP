e3992eb7a3e3b372e945bc086a2a8a910bff6df2
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-12 20:31:20 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -180,7 +180,7 @@ with tab3 :
 
                 # Fusionner le résultat avec combi_risque
                 merged_df = merged_df.merge(combi_risque[['portfolios_volatility']], left_index=True, right_index=True)
-                        
+                st.write('Frontière efficiente')
                 st.scatter_chart(merged_df, x='portfolios_volatility', y='portfolio_returns')
 
             elif nb_acts == 3:
@@ -215,7 +215,42 @@ with tab3 :
 
                 # Fusionner le résultat avec combi_risque
                 merged_df = merged_df.merge(combi_risque[['portfolios_volatility']], left_index=True, right_index=True)
-                        
+                st.write('Frontière efficiente')   
+                st.scatter_chart(merged_df, x='portfolios_volatility', y='portfolio_returns')
+
+            elif nb_acts == 4:
+                file_poids = f"data/poids/{nb_acts}.csv"
+                combi_poids = pd.read_csv(file_poids)   
+                # Créer une matrice de pondérations
+                matrice_poids = combi_poids.values
+                matrice_poids = matrice_poids[:,1:]
+                combi_poids = pd.DataFrame(matrice_poids)
+
+                 #matrice de covariance
+                variation = ptf_df.pct_change().dropna()
+                cov_matrix = variation.cov()/nb_acts
+                cov_matrix['sum'] = cov_matrix.sum(axis=1)
+
+                variation_list = variation.mean().tolist()
+                combi_renta = combi_poids * np.array(variation_list)
+                combi_renta['portfolio_returns'] = combi_renta.sum(axis=1)*100
+
+                # Multiplier chaque matrice de poids par la matrice de covariance
+                matrices_resultats = [np.dot(matrice_poids[i], cov_matrix) for i in range(len(matrice_poids))]
+
+                # Ajouter une colonne supplémentaire pour la somme de chaque ligne
+                for i, matrice_resultat in enumerate(matrices_resultats):
+                    combi_poids.loc[i, 'portfolios_volatility'] = matrice_resultat.sum()
+                combi_risque = np.sqrt(combi_poids) * 100
+
+                combi_poids = combi_poids.drop(columns=['portfolios_volatility'])
+
+                # Fusionner combi_poids avec combi_renta
+                merged_df = combi_poids.merge(combi_renta[['portfolio_returns']], left_index=True, right_index=True)
+
+                # Fusionner le résultat avec combi_risque
+                merged_df = merged_df.merge(combi_risque[['portfolios_volatility']], left_index=True, right_index=True)
+                st.write('Frontière efficiente')   
                 st.scatter_chart(merged_df, x='portfolios_volatility', y='portfolio_returns')
 
             else:
