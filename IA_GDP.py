08124afa534778a15f11ed9378e31fd706330bb7
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-12 17:08:35 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -149,8 +149,7 @@ with tab3 :
          with st.spinner('Chargement du calcul'):
             if nb_acts == 2:
                 file_poids = f"data/poids/{nb_acts}.csv"
-                combi_poids = pd.read_csv(file_poids)
-                combi_poids.set_index(combi_poids[0])
+                combi_poids = pd.read_csv(file_poids)   
                 # Créer une matrice de pondérations
                 matrice_poids = combi_poids.values
                 
