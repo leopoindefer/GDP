e4eb7e84fbe7a759648969fb16bd42779c5efefa
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-13 21:25:54 +0100

    Add files via upload

--- fonctions/prophet.py
+++ fonctions/prophet.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 from prophet import Prophet
+from prophet.diagnostics import performance_metrics
 
 def prophet_model(df):
     m = Prophet()
     m.fit(df)
     future = m.make_future_dataframe(periods=365)
     forecast = m.predict(future)
+    df_p = performance_metrics(forecast)
 
-    return forecast
+    return forecast, df_p
