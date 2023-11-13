f44e050eedafd14512b578e106a01c1c74349d86
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-13 22:59:07 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -143,7 +143,7 @@ with tab2:
 
             #ARIMA
             def ARIMA(df_arima):
-                model_arima = ARIMA(df_arima, order=(1,5,0)).fit()
+                model_arima = ARIMA(df_arima, order=(1,1,5)).fit()
                 predict_arima = model_arima.predict()
                 return predict_arima
             predict_arima = ARIMA(df_arima)
