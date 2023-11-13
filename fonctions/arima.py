ad25dd3ae9c9f4535d6d33bf1ab0b98a14fe49cf
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-13 22:55:42 +0100

    Add files via upload

--- fonctions/arima.py
+++ fonctions/arima.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.graphics.tsaplots import plot_predict
-import statsmodels.api as sm
 
 #ARIMA
 def ARIMA(df_arima):
