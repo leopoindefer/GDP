c80cec8515f53dbc8ffea0d7928a315f5885aa6a
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 13:20:10 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -160,8 +160,6 @@ with tab2:
             st.line_chart(data=graph)
             st.write("PROPHET")
             st.write(round(mse_prophet,2))
-            st.write("ARIMA")
-            st.dataframe(predict_arima)
        
         col1, col2 = st.columns(2)
         with col1:
