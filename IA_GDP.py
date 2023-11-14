721bcf7b9192dae68c5b583ec7473ca2dcc6331f
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 14:17:03 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import datetime
 from datetime import date
+from patsy import dmatrices
+import statsmodels.api as sm
 
 from fonctions.tableau import Tableau
 from fonctions.comparaison import Comparaison
