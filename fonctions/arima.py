826c620539f209b6d3a0d49af1c7b2101f5bdff0
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 08:03:55 +0100

    Add files via upload

--- fonctions/arima.py
+++ fonctions/arima.py
@@ -3,6 +3,7 @@ from statsmodels.tsa.arima.model import ARIMA
 
 #ARIMA
 def model_arima(df_arima):
-        model_arima = ARIMA(df_arima, order=(1,5,0)).fit()
+        arima_shifted = df_arima.shift(2)/df_arima
+        model_arima = ARIMA(arima_shifted, order=(1,5,0)).fit()
         predict_arima = model_arima.predict()
         return predict_arima
