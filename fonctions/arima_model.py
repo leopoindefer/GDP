3bb950af34076d46f4bfbfc24b7913365ece3a62
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 14:08:58 +0100

    Add files via upload

--- fonctions/arima_model.py
+++ fonctions/arima_model.py
@@ -19,12 +19,12 @@ def model_arima(df_arima):
                 for d in d_values:
                         for q in q_values:
                                 try:
-                                        model = ARIMA(arima_shifted, order=(p,d,q)).fit()
+                                        model = ARIMA(df_arima, order=(p,d,q)).fit()
                                         # Stocker l'AIC pour ce modèle
                                         aic_results.append((p,d,q,model.aic))
                                 except:
                                         continue
         best_model = list(sorted(aic_results, key=lambda x: x[3])[0])[0:3]
-        model_arima = ARIMA(arima_shifted, order=best_model).fit()
+        model_arima = ARIMA(df_arima, order=best_model).fit()
         predict_arima = model_arima.forecast()
         return predict_arima
