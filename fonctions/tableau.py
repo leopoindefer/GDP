1e753af38c0fa260926cae1b2b910d1f819c0401
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 19:19:28 +0100

    Add files via upload

--- fonctions/tableau.py
+++ fonctions/tableau.py
@@ -11,28 +11,28 @@ def Tableau(periode, symbol_txt, symbol):
                 file_path = f"data/actions/{s}.csv"
                 s = pd.read_csv(file_path)
                 symbol_dataframes.append(s)
-            except FileNotFoundError:
+                s["Date"] = pd.to_datetime(s["Date"])
+                s = s.set_index("Date")
+                s_resampled = s.resample("M").first()
+                close_columns = [col for col in s_resampled.columns if 'Close' in col]
+                if close_columns:
+                    cours = round(s[close_columns].iloc[-1].values.sum(),2)
+                    cours_prec = round(s_resampled[close_columns].iloc[-2].values.sum(),2)
+                    var = round(((cours - cours_prec)/ cours_prec)*100,2)
+                    mois_prec = datetime.now() - timedelta(days=31)
+                    s_mois_prec = s[s.index>=mois_prec]
+                    s_mois_prec_resampled = s_resampled[s_resampled.index>=mois_prec]
+                    variation = s_mois_prec_resampled[close_columns].pct_change().dropna()
+                    renta_moy = variation.values.mean()
+                    renta_moy = round(renta_moy*100,2)
+                    risque_moy = np.std(s_mois_prec_resampled[close_columns]).values.mean()
+                    risque_moy = round(risque_moy,2)
+                    line = [str(val) for val in s_mois_prec[close_columns].values.flatten()]
+                    liste_cours.append({"SYMBOLE": s_txt, "ACTUEL": f'{cours}', "M-1": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}', "VISION":line})
+            except:
                 continue
-            s["Date"] = pd.to_datetime(s["Date"])
-            s = s.set_index("Date")
-            s_resampled = s.resample("M").first()
-            close_columns = [col for col in s_resampled.columns if 'Close' in col]
-            if close_columns:
-                cours = round(s[close_columns].iloc[-1].values.sum(),2)
-                cours_prec = round(s_resampled[close_columns].iloc[-2].values.sum(),2)
-                var = round(((cours - cours_prec)/ cours_prec)*100,2)
-                mois_prec = datetime.now() - timedelta(days=31)
-                s_mois_prec = s[s.index>=mois_prec]
-                s_mois_prec_resampled = s_resampled[s_resampled.index>=mois_prec]
-                variation = s_mois_prec_resampled[close_columns].pct_change().dropna()
-                renta_moy = variation.values.mean()
-                renta_moy = round(renta_moy*100,2)
-                risque_moy = np.std(s_mois_prec_resampled[close_columns]).values.mean()
-                risque_moy = round(risque_moy,2)
-                line = [str(val) for val in s_mois_prec[close_columns].values.flatten()]
-                liste_cours.append({"SYMBOLE": s_txt, "ACTUEL": f'{cours}', "M-1": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}', "VISION":line})
-        macro = pd.DataFrame(liste_cours)
-        macro.set_index('SYMBOLE', inplace=True)
+            macro = pd.DataFrame(liste_cours)
+            macro.set_index('SYMBOLE', inplace=True)
         return macro
     
     elif periode == "6 mois":
