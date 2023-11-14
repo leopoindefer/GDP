6f87349017c8601f53606cba6469584827937eaa
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 14:14:30 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -9,7 +9,7 @@ from fonctions.comparaison import Comparaison
 from fonctions.projection import Projection
 from fonctions.cdp import CDP
 from fonctions.prophet import prophet_model
-from fonctions.arima_model import model_arima
+from statsmodels.tsa.arima.model import ARIMA
 
 st.set_page_config(
     page_title="GDP",
@@ -141,8 +141,33 @@ with tab2:
                 mse_prophet = mse_prophet.iloc[-1].tolist()
             except:
                 st.error('pas de résultat pour PROPHET')
-
+                
             #ARIMA
+
+            def model_arima(df_arima):
+                arima_shifted = df_arima.shift(2)/df_arima
+                # Définir les différentes valeurs de p, d et q à tester
+                p_values = range(0, 5)
+                d_values = range(0, 5)
+                q_values = range(0, 5)
+
+                # Stocker les résultats de l'AIC pour chaque combinaison de paramètres
+                aic_results = []
+
+                for p in p_values:
+                        for d in d_values:
+                                for q in q_values:
+                                        try:
+                                                model = ARIMA(arima_shifted, order=(p,d,q)).fit()
+                                                # Stocker l'AIC pour ce modèle
+                                                aic_results.append((p,d,q,model.aic))
+                                        except:
+                                                continue
+                best_model = list(sorted(aic_results, key=lambda x: x[3])[0])[0:3]
+                model_arima = ARIMA(arima_shifted, order=best_model).fit()
+                predict_arima = model_arima.forecast()
+                return predict_arima
+
             try:
                 st.write("2 lignes")
                 predict_arima = model_arima(df_arima)
