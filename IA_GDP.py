07a3459fd34337ad61467e3df9370762e6f2f937
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 21:29:38 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -181,7 +181,7 @@ with tab2:
 with tab3 : 
     st.header("Composer votre portefeuille")
 
-    symbol_df = {sym: pd.read_csv(f"data/actions/{sym}.csv") for sym in symbol_txt}
+    symbol_df = {s: pd.read_csv(f"data/actions/{s}.csv") for s in symbol_txt}
 
     portefeuille = st.multiselect("Choisissez vos actions", symbol_txt)
     nb_acts = len(portefeuille)
