5c2985f06b8650fa5009124a677284bc6fd0ef10
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 12:10:57 +0100

    Add files via upload

--- fonctions/arima.py
+++ fonctions/arima.py
@@ -24,5 +24,5 @@ def model_arima(df_arima):
                                         continue
         best_model = list(sorted(aic_results, key=lambda x: x[3])[0])[0:3]
         model_arima = sm.tsa.ARIMA(arima_shifted, order=best_model).fit()
-        predict_arima = model_arima.get_prediction()
+        predict_arima = model_arima.get_forecast()
         return predict_arima
