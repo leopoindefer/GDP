7c97eab4f5298920a75329f8c395c0a3fc595d9c
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 17:52:32 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -50,7 +50,7 @@ with tab1 :
         indice = st.selectbox("Indices", liste_indice)
         file_indice = f"data/indices/{indice}.csv"
         df_indice = pd.read_csv(file_indice, delimiter=";")
-        actions = list(df_indice.loc[:,["ticker"]])
+        actions = df_indice.loc[-1].tolist()
     st.write(actions)
     macro = Tableau(periode, actions, symbol_dataframes)
     
