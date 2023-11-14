832d19e1b93d19c81a9d01aafa12330ed6a89425
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 14:12:45 +0100

    Add files via upload

--- fonctions/arima_model.py
+++ fonctions/arima_model.py
@@ -2,10 +2,10 @@ from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.statespace import sarimax
 from statsmodels.tsa.arima.specification import SARIMAXSpecification
 from statsmodels.tsa.base.tsa_model import TimeSeriesModel
-import pandas as pd
 
 #ARIMA
-def model_arima(arima_shifted):
+def model_arima(df_arima):
+        arima_shifted = df_arima.shift(2)/df_arima
         # Définir les différentes valeurs de p, d et q à tester
         p_values = range(0, 5)
         d_values = range(0, 5)
