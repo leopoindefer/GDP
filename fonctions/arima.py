9bd16d64dbba82b3f951a08556d37542b7830675
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 13:47:02 +0100

    Add files via upload

--- fonctions/arima.py
+++ fonctions/arima.py
@@ -6,23 +6,26 @@ import statsmodels.api as sm
 def model_arima(df_arima):
         arima_shifted = pd.df_arima.shift(2)/df_arima
         # Définir les différentes valeurs de p, d et q à tester
-        p_values = range(0, 5)
-        d_values = range(0, 5)
-        q_values = range(0, 5)
+        try:
+                p_values = range(0, 5)
+                d_values = range(0, 5)
+                q_values = range(0, 5)
 
-        # Stocker les résultats de l'AIC pour chaque combinaison de paramètres
-        aic_results = []
+                # Stocker les résultats de l'AIC pour chaque combinaison de paramètres
+                aic_results = []
 
-        for p in p_values:
-                for d in d_values:
-                        for q in q_values:
-                                try:
-                                        model = sm.tsa.ARIMA(arima_shifted, order=(p,d,q)).fit()
-                                        # Stocker l'AIC pour ce modèle
-                                        aic_results.append((p,d,q,model.aic))
-                                except:
-                                        continue
-        best_model = list(sorted(aic_results, key=lambda x: x[3])[0])[0:3]
-        model_arima = sm.tsa.ARIMA(arima_shifted, order=best_model).fit()
-        predict_arima = model_arima.predict()
+                for p in p_values:
+                        for d in d_values:
+                                for q in q_values:
+                                        try:
+                                                model = sm.tsa.ARIMA(arima_shifted, order=(p,d,q)).fit()
+                                                # Stocker l'AIC pour ce modèle
+                                                aic_results.append((p,d,q,model.aic))
+                                        except:
+                                                continue
+                best_model = list(sorted(aic_results, key=lambda x: x[3])[0])[0:3]
+                model_arima = sm.tsa.ARIMA(arima_shifted, order=best_model).fit()
+                predict_arima = model_arima.predict()
+        except:
+                ("model")
         return predict_arima
