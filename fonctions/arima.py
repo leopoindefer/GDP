38f655414fe5dd31eb411d6c69512cf629bb6466
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-14 08:08:07 +0100

    Add files via upload

--- fonctions/arima.py
+++ fonctions/arima.py
@@ -1,9 +1,26 @@
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
+import statsmodels.api as sm
 
 #ARIMA
 def model_arima(df_arima):
         arima_shifted = df_arima.shift(2)/df_arima
-        model_arima = ARIMA(arima_shifted, order=(1,5,0)).fit()
+        # Définir les différentes valeurs de p, d et q à tester
+        p_values = range(0, 5)
+        d_values = range(0, 5)
+        q_values = range(0, 5)
+
+        # Stocker les résultats de l'AIC pour chaque combinaison de paramètres
+        aic_results = []
+
+        for p in p_values:
+                for d in d_values:
+                        for q in q_values:
+                                try:
+                                        model = sm.tsa.ARIMA(arima_shifted, order=(p,d,q)).fit()
+                                        # Stocker l'AIC pour ce modèle
+                                        aic_results.append((p,d,q,model.aic))
+                                except:
+                                        continue
         predict_arima = model_arima.predict()
         return predict_arima
