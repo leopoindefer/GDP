7f401aeb292236eaea1ab768da09efb89de9f3c7
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-15 16:14:04 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from datetime import date
+from datetime import datetime
 
 from fonctions.tableau import Tableau
 from fonctions.comparaison import Comparaison
