8262143cd9e1cc1e53dda839b344bf26c38269da
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-15 16:26:15 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -182,7 +182,7 @@ with tab3 :
     portefeuille = st.multiselect("Choisissez vos actions", symbol_txt)
 
 
-    date_index = pd.date_range(start='2000-01-01', end=date.now(), freq='D')
+    date_index = pd.date_range(start='2000-01-01', end=date.today(), freq='D')
 
     # Initialisez le DataFrame résultant avec l'index date
     ptf_df = pd.DataFrame(index=date_index)
