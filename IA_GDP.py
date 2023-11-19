2f1d28a69b6c921fc907a2227b312f39fcd4f1dd
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-11-19 18:17:01 +0100

    Add files via upload

--- IA_GDP.py
+++ IA_GDP.py
@@ -151,8 +151,8 @@ with tab2:
                 loss_prophet = predict_prophet.set_index('ds').join(df_prophet.set_index('ds'), how="left")
                 loss_prophet = loss_prophet.loc[:,["ds","yhat"]]
                 loss_prophet['se'] = np.square(loss_prophet['y'] - loss_prophet['yhat'])
-                mse_prophet = loss_prophet.mean(axis=0)
-                mse_prophet = mse_prophet.iloc[-1].tolist()
+                mse_prophet = loss_prophet['se'].mean(axis=0)
+                mse_prophet = mse_prophet['se'].iloc[-1].tolist()
             except:
                 st.error('pas de résultat pour PROPHET')
                 
