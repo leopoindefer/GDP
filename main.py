482dc88dff9add38c64e2e6448b822ff2ecd2c61
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-01 15:19:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ with tab1 :
 
     col_vision1, col_vision2, col_vision3 = st.columns((2,3,5))
     with col_vision1:
-        periode = st.selectbox("Période d'analyse", ["1 mois","6 mois","1 an","5 ans"])
+        periode = st.selectbox("Période d'analyse", ["6 mois"])
     with col_vision2:
         indice = st.selectbox("Indice", liste_indice)
     with col_vision3:
@@ -43,14 +43,14 @@ with tab1 :
     st.write(selected_dataframes)
     #if periode == "1 mois":
         #tableau = Analyse(selected_dataframes).KPI_1month()
-    #elif periode == "6 mois":
-        #tableau = Analyse(selected_dataframes).KPI_6month()
+    if periode == "6 mois":
+        tableau = Analyse(selected_dataframes).KPI_6month()
     #elif periode == "1 an":
         #tableau = Analyse(selected_dataframes).KPI_1year()
     #elif periode == "5 ans":
         #tableau = Analyse(selected_dataframes).KPI_5year()
-    #st.dataframe(tableau.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']), column_config={"VISION": st.column_config.LineChartColumn(
-        #"VISION", y_min=0, y_max=200)})
+    st.dataframe(tableau.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']), column_config={"VISION": st.column_config.LineChartColumn(
+        "VISION", y_min=0, y_max=200)})
 
     col1_info, col2_info = st.columns((1, 8))
     with col1_info:
