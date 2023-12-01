f7085a8ab309672419b187a1354007af9a6098b5
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-01 14:59:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ with tab1 :
     st.markdown(hide_st_style, unsafe_allow_html=True) 
 
     liste_indice = ["CAC40", "DOWJONES", "NASDAQ100", "S&P500", "SBF120"]
-    st.write(liste_indice)
-    
+
     col_vision1, col_vision2, col_vision3 = st.columns((2,3,5))
     with col_vision1:
         periode = st.selectbox("Période d'analyse", ["1 mois","6 mois","1 an","5 ans"])
@@ -41,19 +40,17 @@ with tab1 :
     #assets_all = Library(liste_indice, None).get_assets()
     assets = Library(indice,None).get_assets()
     selected_dataframes = Library(None,assets).get_dataframes()
-    try:
-        if periode == "1 mois":
-            tableau = Analyse(selected_dataframes).KPI_1month()
-        elif periode == "6 mois":
-            tableau = Analyse(selected_dataframes).KPI_6month()
-        elif periode == "1 an":
-            tableau = Analyse(selected_dataframes).KPI_1year()
-        elif periode == "5 ans":
-            tableau = Analyse(selected_dataframes).KPI_5year()
-        st.dataframe(tableau.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']), column_config={"VISION": st.column_config.LineChartColumn(
-            "VISION", y_min=0, y_max=200)})
-    except:
-        st.write("pas de donnée")
+
+    if periode == "1 mois":
+        tableau = Analyse(selected_dataframes).KPI_1month()
+    elif periode == "6 mois":
+        tableau = Analyse(selected_dataframes).KPI_6month()
+    elif periode == "1 an":
+        tableau = Analyse(selected_dataframes).KPI_1year()
+    elif periode == "5 ans":
+        tableau = Analyse(selected_dataframes).KPI_5year()
+    st.dataframe(tableau.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']), column_config={"VISION": st.column_config.LineChartColumn(
+        "VISION", y_min=0, y_max=200)})
 
     col1_info, col2_info = st.columns((1, 8))
     with col1_info:
