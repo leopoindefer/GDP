d98455d789a469a25aa5c754918f9a02748c7942
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-01 14:48:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -94,11 +94,10 @@ with tab1 :
         comp1 = symb1
         comp2 = symb2
         assets_comp = [comp1, comp2]
-        st.write(comp1)
-        #selected_dataframes = Library(None, assets_comp)
-        #dataframes_resampled = Transform(selected_dataframes)
-        #compar_chart, corr = Comparaison(dataframes_resampled).inner_combine()
-        #st.line_chart(compar_chart)
+        selected_dataframes = Library(None, assets_comp)
+        dataframes_resampled = Transform(selected_dataframes)
+        compar_chart, corr = Comparaison(dataframes_resampled).inner_combine()
+        st.line_chart(compar_chart)
 
         #mess_corr = f'Corrélation linéraire à : {round(corr*100,2)}%'
         #st.write(mess_corr)
