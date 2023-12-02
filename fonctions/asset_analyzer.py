09a8f0897145952743d0dfb7f863e2323058d792
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-02 23:23:17 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -53,6 +53,12 @@ class Analyse(Transform):
                     s_resampled = s.resample("M").first()
                     close_columns = [col for col in s_resampled.columns if 'Close' in col]
                     if close_columns:
+                        cours = []
+                        cours_prec = []
+                        var = []
+                        renta_moy = []
+                        risque_moy = []
+                        line = []
                         cours = round(s[close_columns].iloc[-1].values.sum(),2)
                         cours_prec = round(s_resampled[close_columns].iloc[-7].values.sum(),2)
                         var = round(((cours - cours_prec)/ cours_prec)*100,2)
