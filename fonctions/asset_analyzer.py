778768930dd746b5dc6aac9ab6be7d08e1c5d739
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-02 23:05:14 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -37,26 +37,22 @@ class Analyse(Transform):
 
     def KPI_6month(self):
         liste_cours = []
-        asset_dataframe_resampled = {}
         liste_symb = []
         liste_df = []
         for symbol, asset_dataframe in self._selected_dataframes.items():
             liste_symb.append(symbol)
             liste_df.append(asset_dataframe)
-            try:
-                for df in liste_df:
-                    close_columns = [col for col in df.columns if 'Close' in col]
+            for s in liste_df:
+                try:      
+                    s_resampled = s.resample("M").first()
+                    close_columns = [col for col in s_resampled.columns if 'Close' in col]
                     if close_columns:
-                        df_resampled = Transform(df).resample().values()
-                        df_resampled = pd.DataFrame(df_resampled)
-                        cours = round(df[close_columns].iloc[-1].values.sum(),2)
-                        cours_prec = round(df_resampled[close_columns].iloc[-7].values.sum(),2)
+                        cours = round(s[close_columns].iloc[-1].values.sum(),2)
+                        cours_prec = round(s_resampled[close_columns].iloc[-7].values.sum(),2)
                         var = round(((cours - cours_prec)/ cours_prec)*100,2)
                         sixmois_prec = datetime.now() - timedelta(days=183)
-                        sixmois_prec = pd.to_datetime(sixmois_prec)
-                        df.index = pd.to_datetime(df.index)
-                        s_six_mois_prec = df[df.index>=sixmois_prec]
-                        s_six_mois_prec_resampled = df_resampled[df_resampled.index>=sixmois_prec]
+                        s_six_mois_prec = s[s.index>=sixmois_prec]
+                        s_six_mois_prec_resampled = s_resampled[s_resampled.index>=sixmois_prec]
                         variation = s_six_mois_prec_resampled[close_columns].pct_change().dropna()
                         renta_moy = variation.values.mean()
                         renta_moy = round(renta_moy*100,2)
@@ -64,11 +60,13 @@ class Analyse(Transform):
                         risque_moy = round(risque_moy*100,2)
                         line = [str(val) for val in s_six_mois_prec[close_columns].values.flatten()]
                         liste_cours.append({"SYMBOLE": liste_symb, "ACTUEL": f'{cours}', "M-6": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
-            except:
-                continue
+                except FileNotFoundError:
+                    continue
+                except Exception:
+                    continue
         macro = pd.DataFrame(liste_cours)
-        #macro.set_index('SYMBOLE', inplace=True)
-        return var
+        macro.set_index('SYMBOLE', inplace=True)
+        return macro
 
     def KPI_1year(self):
         liste_cours = list()
