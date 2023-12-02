f372aa45fc223dd74d5329ffb35d62575849f159
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-02 21:53:48 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -56,14 +56,14 @@ class Analyse(Transform):
                 risque_moy = variation.values.std()
                 risque_moy = round(risque_moy*100,2)
                 line = [str(val) for val in s_six_mois_prec[close_columns].values.flatten()]
-                liste_cours.append({"SYMBOLE": symbol, "ACTUEL": f'{cours}', "M-6": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
+                liste_cours.append({"SYMBOLE": liste_symb, "ACTUEL": f'{cours}', "M-6": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
             except FileNotFoundError:
                 continue
             except Exception:
                 continue
-        #macro = pd.DataFrame(liste_cours)
-        #macro.set_index('SYMBOLE', inplace=True)
-        return liste_symb
+        macro = pd.DataFrame(liste_cours)
+        macro.set_index('SYMBOLE', inplace=True)
+        return macro
 
     def KPI_1year(self):
         liste_cours = list()
