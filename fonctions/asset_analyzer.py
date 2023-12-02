0d57c9348a9652319d135db35bdb69eb93054e0a
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-02 19:42:45 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -61,7 +61,7 @@ class Analyse(Transform):
                 continue
         macro = pd.DataFrame(liste_cours)
         macro.set_index('SYMBOLE', inplace=True)
-        return macro
+        return symbol
 
     def KPI_1year(self):
         liste_cours = list()
