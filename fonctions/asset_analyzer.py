88288781ae0c782dd5e1fadb14e0e772782ce071
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 14:38:10 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -39,12 +39,13 @@ class Analyse(Transform):
         liste_cours = []
         for symbol in self._selected_dataframes.keys():
             liste_cours.append({"SYMBOLE": symbol})
-        for asset_dataframe in self._selected_dataframes.values(): 
+        for asset_dataframe in self._selected_dataframes.values():
             s = pd.DataFrame(asset_dataframe)
             s.index = pd.to_datetime(s.index)
             s_resampled = s.resample("M").first()
             close_columns = [col for col in s_resampled.columns if 'Close' in col]
             cours = round(s[close_columns].iloc[-1].values.sum(),2)
+            liste_cours.append({"SYMBOLE": cours})
             #cours_prec = round(s_resampled[close_columns].iloc[-7].values.sum(),2)
             #var = round(((cours - cours_prec)/ cours_prec)*100,2)
             #sixmois_prec = datetime.now() - timedelta(days=183)
@@ -59,7 +60,7 @@ class Analyse(Transform):
             #liste_cours.append({"ACTUEL": f'{cours}', "M-6": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
         #macro = pd.DataFrame(liste_cours)
         #macro.set_index('SYMBOLE', inplace=True)
-        return cours
+        return liste_cours
 
     def KPI_1year(self):
         liste_cours = list()
