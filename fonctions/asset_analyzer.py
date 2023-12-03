15fc0da757506a9dfb21b4a7e0185488832d4d0e
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 13:50:35 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -61,8 +61,9 @@ class Analyse(Transform):
                 continue
             except Exception:
                 continue
+            liste_cours.append({"ACTUEL": f'{cours}', "M-6": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
         macro = pd.DataFrame(liste_cours)
-        #macro.set_index('SYMBOLE', inplace=True)
+        macro.set_index('SYMBOLE', inplace=True)
         return macro
 
     def KPI_1year(self):
