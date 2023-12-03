d046110d4705e4a3dc2736b2221095e132d8dbef
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 18:08:42 +0100

    Add files via upload

--- fonctions/asset_projection.py
+++ fonctions/asset_projection.py
@@ -12,6 +12,8 @@ class Projection():
     def unit_projection(self):
         montant = float(self._montant)
         duree = pd.to_datetime(self._duree)
+        confiance_pred = self._forecast[self._forecast["date"] == duree]
+        confiance_pred = confiance_pred.loc[:,["yhat_lower","yhat_upper"]]
         try:
             ticker = yf.Ticker(self._asset)
             div_indiv = pd.DataFrame(ticker.dividends)
@@ -25,25 +27,41 @@ class Projection():
             start_value = self._df["y"].iloc[-1]
             end_date = self._forecast[self._forecast['date'] <= duree]
             end_value = end_date["forecast"].iloc[-1]
+            lower_endvalue = confiance_pred["yhat_lower"].iloc[-1]
+            upper_endvalue = confiance_pred["yhat_upper"].iloc[-1]
+            gap_indiv_lower = (lower_endvalue - start_value)
+            gap_indiv_upper = (upper_endvalue - start_value)
             gap_indiv_value = (end_value - start_value)
             invest_part = float(montant/start_value)
             div = div_indiv * invest_part
+            gap_value_lower = gap_indiv_lower * invest_part
+            gap_value_upper = gap_indiv_upper * invest_part
             gap_value = gap_indiv_value * invest_part
             tRend = (div/montant)*100
             tRent = (div+gap_value)/montant*100
+            tRent_lower = (div+gap_value_lower)/montant*100
+            tRent_upper = (div+gap_value_upper)/montant*100
             if tRent<0:
-                nb_part = f'<span style="color: #008000;">{round(invest_part,2)}</span>'
+                nb_part = f'<span style="color: #FF0000;">{round(invest_part,2)}</span>'
                 tx_rendement = f'<span style="color: #008000;">{round(tRend,2)}%</span>'
-                rendement = f'<span style="color: #008000;">{round(div,2)}€</span>'
-                tx_rentabilite = f'<span style="color: #008000;">{round(tRent,2)}%</span>' 
-                rentabilite = f'<span style="color: #008000;">{round(div + gap_value,2)}€</span>'
+                rendement = f'<span style="color: #FF0000;">{round(div,2)}€</span>'
+                tx_rentabilite = f'<span style="color: #FF0000;">{round(tRent,2)}%</span>' 
+                rentabilite = f'<span style="color: #FF0000;">{round(div + gap_value,2)}€</span>'
+                tx_renta_lower = f'<span style="color: #FF0000;">{round(tRent_lower,2)}%</span>' 
+                renta_lower = f'<span style="color: #FF0000;">{round(div + gap_value_lower,2)}€</span>'
+                tx_renta_upper = f'<span style="color: #FF0000;">{round(tRent_upper,2)}%</span>' 
+                renta_upper = f'<span style="color: #FF0000;">{round(div + gap_value_upper,2)}€</span>'
             elif tRent>=0:
                 nb_part = f'<span style="color: #008000;">{round(invest_part,2)}</span>'
                 tx_rendement = f'<span style="color: #008000;">{round(tRend,2)}%</span>'
                 rendement = f'<span style="color: #008000;">{round(div,2)}€</span>'
                 tx_rentabilite = f'<span style="color: #008000;">{round(tRent,2)}%</span>' 
                 rentabilite = f'<span style="color: #008000;">{round(div + gap_value,2)}€</span>'
+                tx_renta_lower = f'<span style="color: #008000;">{round(tRent_lower,2)}%</span>' 
+                renta_lower = f'<span style="color: #008000;">{round(div + gap_value_lower,2)}€</span>'
+                tx_renta_upper = f'<span style="color: #008000;">{round(tRent_upper,2)}%</span>' 
+                renta_upper = f'<span style="color: #008000;">{round(div + gap_value_upper,2)}€</span>'
         except:
             print('pas de résultat')
 
-        return  nb_part, tx_rendement, rendement, tx_rentabilite, rentabilite
+        return  nb_part, tx_rendement, rendement, tx_rentabilite, rentabilite, tx_renta_lower, renta_lower, tx_renta_upper, renta_upper
