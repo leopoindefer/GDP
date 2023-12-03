2b941ba0161d68d210303f85fcd0e90a8e262b81
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 13:58:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ with tab1 :
     assets = Library(indice,None).get_assets()
     #assets_all = Library(liste_indice, None).get_assets()
     dataframes = Library(None,assets).get_dataframes()
-    tableau = Analyse(dataframes).KPI_6month()
+    tableau = Analyse(["META","AAPL"]).KPI_6month()
     st.write(tableau)
     #st.dataframe(tableau.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']), column_config={"VISION": st.column_config.LineChartColumn(
         #"VISION", y_min=0, y_max=200)})
