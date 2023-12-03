9e8dde3f2d5faade412580cc6025a1b3d76fa31d
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 15:51:49 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -8,36 +8,45 @@ class Analyse(Transform):
         super().__init__(selected_dataframes)
 
     def KPI_1month(self):
-        liste_cours = list()
+        liste_cours = []
         for symbol, asset_dataframe in self._selected_dataframes.items():
             try:
                 asset_dataframe_resampled = Transform(asset_dataframe).resample()
                 close_columns = [col for col in asset_dataframe_resampled.columns if 'Close' in col]
-                if close_columns:
-                    cours = round(asset_dataframe[close_columns].iloc[-1].values.sum(),2)
-                    cours_prec = round(asset_dataframe_resampled[close_columns].iloc[-2].values.sum(),2)
-                    var = round(((cours - cours_prec)/ cours_prec)*100,2)
-                    mois_prec = datetime.now() - timedelta(days=31)
-                    s_mois_prec = asset_dataframe[asset_dataframe.index>=mois_prec]
-                    s_mois_prec_resampled = asset_dataframe_resampled[asset_dataframe_resampled.index>=mois_prec]
-                    variation = s_mois_prec_resampled[close_columns].pct_change().dropna()
-                    renta_moy = variation.values.mean()
-                    renta_moy = round(renta_moy*100,2)
-                    risque_moy = variation.values.std()
-                    risque_moy = round(risque_moy*100,2)
-                    line = [str(val) for val in s_mois_prec[close_columns].values.flatten()]
-                    liste_cours.append({"SYMBOLE": symbol, "ACTUEL": f'{cours}', "M-1": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
+                cours = round(asset_dataframe[close_columns].iloc[-1].values.sum(),2)
+                cours_prec = round(asset_dataframe_resampled[close_columns].iloc[-2].values.sum(),2)
+                var = round(((cours - cours_prec)/ cours_prec)*100,2)
+                mois_prec = datetime.now() - timedelta(days=31)
+                s_mois_prec = asset_dataframe[asset_dataframe.index>=mois_prec]
+                s_mois_prec_resampled = asset_dataframe_resampled[asset_dataframe_resampled.index>=mois_prec]
+                variation = s_mois_prec_resampled[close_columns].pct_change().dropna()
+                renta_moy = variation.values.mean()
+                renta_moy = round(renta_moy*100,2)
+                risque_moy = variation.values.std()
+                risque_moy = round(risque_moy*100,2)
+                line = [str(val) for val in s_mois_prec[close_columns].values.flatten()]
+                
+                liste_cours.append({
+                    "SYMBOL": symbol,
+                    "ACTUEL": f'{cours}',
+                    "M-6": f'{cours_prec}',
+                    "VAR" : f'{var}%',
+                    "RENTABILITÉ": f'{renta_moy}%', 
+                    "VOLATILITÉ": f'{risque_moy}%',
+                    "VISION":line
+                })
+            
             except FileNotFoundError:
                 continue
             except Exception:
                 continue
+
         macro = pd.DataFrame(liste_cours)
         macro.set_index('SYMBOLE', inplace=True)
         return macro
 
     def KPI_6month(self):
         liste_cours = []
-        liste_variation = []
         for symbol, asset_dataframe in self._selected_dataframes.items():
             try: 
                 s = pd.DataFrame(asset_dataframe)
@@ -51,7 +60,6 @@ class Analyse(Transform):
                 s_six_mois_prec = s[s.index>=sixmois_prec]
                 s_six_mois_prec_resampled = s_resampled[s_resampled.index>=sixmois_prec]
                 variation = s_six_mois_prec_resampled[close_columns].pct_change().dropna()
-                liste_variation.append(variation)
                 renta_moy = variation.values.mean()
                 renta_moy = round(renta_moy*100,2)
                 risque_moy = variation.values.std()
@@ -77,33 +85,42 @@ class Analyse(Transform):
         macro.set_index('SYMBOL', inplace=True)
         return macro
 
-
     def KPI_1year(self):
-        liste_cours = list()
+        liste_cours = []
         for symbol, asset_dataframe in self._selected_dataframes.items():
             try:
                 asset_dataframe_resampled = Transform(asset_dataframe).resample()
                 close_columns = [col for col in asset_dataframe_resampled.columns if 'Close' in col]
-                if close_columns:
-                    cours = round(asset_dataframe[close_columns].iloc[-1].values.sum(),2)
-                    cours_prec = round(asset_dataframe_resampled[close_columns].iloc[-13].values.sum(),2)
-                    var = round(((cours - cours_prec)/ cours_prec)*100,2)
-                    annee_prec = datetime.now() - timedelta(days=365)
-                    annee_prec = pd.to_datetime(annee_prec)
-                    asset_dataframe.index = pd.to_datetime(asset_dataframe.index)
-                    s_annee_prec = asset_dataframe[asset_dataframe.index >= annee_prec]
-                    s_annee_prec_resampled = asset_dataframe_resampled[asset_dataframe_resampled.index>=annee_prec]
-                    variation = s_annee_prec_resampled[close_columns].pct_change().dropna()
-                    renta_moy = variation.values.mean()
-                    renta_moy = round(renta_moy*100,2)
-                    risque_moy = variation.values.std()
-                    risque_moy = round(risque_moy*100,2)
-                    line = [str(val) for val in s_annee_prec[close_columns].values.flatten()]
-                    liste_cours.append({"SYMBOLE": symbol, "ACTUEL": f'{cours}', "N-1": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
+                cours = round(asset_dataframe[close_columns].iloc[-1].values.sum(),2)
+                cours_prec = round(asset_dataframe_resampled[close_columns].iloc[-13].values.sum(),2)
+                var = round(((cours - cours_prec)/ cours_prec)*100,2)
+                annee_prec = datetime.now() - timedelta(days=365)
+                annee_prec = pd.to_datetime(annee_prec)
+                asset_dataframe.index = pd.to_datetime(asset_dataframe.index)
+                s_annee_prec = asset_dataframe[asset_dataframe.index >= annee_prec]
+                s_annee_prec_resampled = asset_dataframe_resampled[asset_dataframe_resampled.index>=annee_prec]
+                variation = s_annee_prec_resampled[close_columns].pct_change().dropna()
+                renta_moy = variation.values.mean()
+                renta_moy = round(renta_moy*100,2)
+                risque_moy = variation.values.std()
+                risque_moy = round(risque_moy*100,2)
+                line = [str(val) for val in s_annee_prec[close_columns].values.flatten()]
+
+                liste_cours.append({
+                    "SYMBOL": symbol,
+                    "ACTUEL": f'{cours}',
+                    "M-6": f'{cours_prec}',
+                    "VAR" : f'{var}%',
+                    "RENTABILITÉ": f'{renta_moy}%', 
+                    "VOLATILITÉ": f'{risque_moy}%',
+                    "VISION":line
+                })
+
             except FileNotFoundError:
                 continue
             except Exception:
                 continue
+
         macro = pd.DataFrame(liste_cours)
         macro.set_index('SYMBOLE', inplace=True)
         return macro
@@ -114,23 +131,33 @@ class Analyse(Transform):
             try:
                 asset_dataframe_resampled = Transform(asset_dataframe).resample()
                 close_columns = [col for col in asset_dataframe_resampled.columns if 'Close' in col]
-                if close_columns:
-                    cours = round(asset_dataframe[close_columns].iloc[-1].values.sum(),2)
-                    start_date = datetime.now() - timedelta(days=365*5)
-                    s_cinq_ans = asset_dataframe_resampled[asset_dataframe_resampled.index>=start_date]
-                    cours_prec = round(s_cinq_ans[close_columns].iloc[0].values.sum(),2)
-                    var = round(((cours - cours_prec)/ cours_prec)*100,2)
-                    variation = s_cinq_ans[close_columns].pct_change().dropna()
-                    renta_moy = variation.values.mean()
-                    renta_moy = round(renta_moy*100,2)
-                    risque_moy = variation.values.std()
-                    risque_moy = round(risque_moy*100,2)
-                    line = [str(val) for val in s_cinq_ans[close_columns].values.flatten()]
-                    liste_cours.append({"SYMBOLE": symbol, "ACTUEL": f'{cours}', "N-5": f'{cours_prec}', "VAR": f'{var}%', "RENTABILITÉ": f'{renta_moy}%', "VOLATILITÉ": f'{risque_moy}%', "VISION":line})
+                cours = round(asset_dataframe[close_columns].iloc[-1].values.sum(),2)
+                start_date = datetime.now() - timedelta(days=365*5)
+                s_cinq_ans = asset_dataframe_resampled[asset_dataframe_resampled.index>=start_date]
+                cours_prec = round(s_cinq_ans[close_columns].iloc[0].values.sum(),2)
+                var = round(((cours - cours_prec)/ cours_prec)*100,2)
+                variation = s_cinq_ans[close_columns].pct_change().dropna()
+                renta_moy = variation.values.mean()
+                renta_moy = round(renta_moy*100,2)
+                risque_moy = variation.values.std()
+                risque_moy = round(risque_moy*100,2)
+                line = [str(val) for val in s_cinq_ans[close_columns].values.flatten()]
+
+                liste_cours.append({
+                    "SYMBOL": symbol,
+                    "ACTUEL": f'{cours}',
+                    "M-6": f'{cours_prec}',
+                    "VAR" : f'{var}%',
+                    "RENTABILITÉ": f'{renta_moy}%', 
+                    "VOLATILITÉ": f'{risque_moy}%',
+                    "VISION":line
+                })
+                
             except FileNotFoundError:
                 continue
             except Exception:
                 continue
+
         macro = pd.DataFrame(liste_cours)
         macro.set_index('SYMBOLE', inplace=True)
         return macro
