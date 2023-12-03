c062cc059fe85395d8165e2905e0c5210ec834fe
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 20:28:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -130,46 +130,47 @@ with tab3 :
 
             if nb_acts <= 4:
                 portfolio_dataframes = Library(portefeuille).get_dataframes()
-                merged_df, df_RisqueFaible, df_RisqueMoyen, df_RisqueEleve, df_RisqueTresEleve = Optimize(portefeuille, nb_acts, portfolio_dataframes).get_optimum()
-                st.subheader('Frontière efficiente')
-                st.scatter_chart(merged_df, x='Volatilité', y='Rentabilité')
+                st.write(portfolio_dataframes)
+                #merged_df, df_RisqueFaible, df_RisqueMoyen, df_RisqueEleve, df_RisqueTresEleve = Optimize(portefeuille, nb_acts, portfolio_dataframes).get_optimum()
+                #st.subheader('Frontière efficiente')
+                #st.scatter_chart(merged_df, x='Volatilité', y='Rentabilité')
                     #st.dataframe(merged_df, hide_index=True)
-                st.subheader('Portefeuille efficient pour :')
-                col_result1, col_result2, col_result3, col_result4 = st.columns(4)
-                with col_result1:
-                    st.write("Risque faible")
-                    try :
-                        RisqueFaible = df_RisqueFaible.iloc[0]
-                        st.dataframe(RisqueFaible)
-                    except:
-                        st.error("Aucun")
+                #st.subheader('Portefeuille efficient pour :')
+                #col_result1, col_result2, col_result3, col_result4 = st.columns(4)
+                #with col_result1:
+                    #st.write("Risque faible")
+                    #try :
+                        #RisqueFaible = df_RisqueFaible.iloc[0]
+                        #st.dataframe(RisqueFaible)
+                    #except:
+                        #st.error("Aucun")
 
-                with col_result2:
-                    st.write("Risque moyen")
-                    try:
-                        RisqueMoyen = df_RisqueMoyen.iloc[0]
-                        st.dataframe(RisqueMoyen)
-                    except:
-                        st.error("Aucun")
+                #with col_result2:
+                    #st.write("Risque moyen")
+                    #try:
+                        #RisqueMoyen = df_RisqueMoyen.iloc[0]
+                        #st.dataframe(RisqueMoyen)
+                    #except:
+                        #st.error("Aucun")
 
-                with col_result3:
-                    st.write("Risque élevé")
-                    try:
-                        RisqueEleve = df_RisqueEleve.iloc[0]
-                        st.dataframe(RisqueEleve)
-                    except:
-                        st.error("Aucun")
+                #with col_result3:
+                    #st.write("Risque élevé")
+                    #try:
+                        #RisqueEleve = df_RisqueEleve.iloc[0]
+                        #st.dataframe(RisqueEleve)
+                    #except:
+                        #st.error("Aucun")
 
-                with col_result4:
-                    st.write("Risque très élevé")
-                    try:
-                        RisqueTresEleve = df_RisqueTresEleve.iloc[0]
-                        st.dataframe(RisqueTresEleve)
-                    except:
-                        st.error("Aucun")
+                #with col_result4:
+                    #st.write("Risque très élevé")
+                    #try:
+                        #RisqueTresEleve = df_RisqueTresEleve.iloc[0]
+                        #st.dataframe(RisqueTresEleve)
+                    #except:
+                        #st.error("Aucun")
 
-            else:
-                mess_gdp = f"Création de portefeuille pas encore disponible pour {nb_acts}"
-                st.write(mess_gdp)
+            #else:
+                #mess_gdp = f"Création de portefeuille pas encore disponible pour {nb_acts}"
+                #st.write(mess_gdp)
             #except:
                 #st.error("Pas de portefeuille possible")
