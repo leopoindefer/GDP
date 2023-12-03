bea30f500ec20b6221a9f14d76b3701d62f5a0c6
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-03 19:44:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -109,12 +109,11 @@ with tab2:
     with col2:
         duree = st.date_input("Jusqu'à quand ?", pd.to_datetime(forecast["date"].iloc[-1]), min_value=pd.to_datetime(df_prophet["ds"].iloc[0]), max_value=pd.to_datetime(forecast["date"].iloc[-1]))
 
-    nb_part = Projection(montant, duree, asset, df_prophet, forecast).unit_projection()
-    st.write(nb_part)
-    #st.write(f'Nombre d action acheté : {nb_part}', unsafe_allow_html=True)
-    #st.write(f'Taux de rendement de : {tx_rendement}, Rendement de {rendement}', unsafe_allow_html=True)
-    #st.write(f'Taux de Rentabilité de : {tx_rentabilite}, Rentabilité de {rentabilite}', unsafe_allow_html=True)
-    #st.write(f'Intervalle de confiance de rentabilité : [{renta_lower} : {renta_upper}]', unsafe_allow_html=True)
+    nb_part, tx_rendement, rendement, tx_rentabilite, rentabilite, tx_renta_lower, renta_lower, tx_renta_upper, renta_upper = Projection(montant, duree, asset, df_prophet, forecast).unit_projection()
+    st.write(f'Nombre d action acheté : {nb_part}', unsafe_allow_html=True)
+    st.write(f'Taux de rendement de : {tx_rendement}, Rendement de {rendement}', unsafe_allow_html=True)
+    st.write(f'Taux de Rentabilité de : {tx_rentabilite}, Rentabilité de {rentabilite}', unsafe_allow_html=True)
+    st.write(f'Intervalle de confiance de rentabilité : [{renta_lower} : {renta_upper}]', unsafe_allow_html=True)
 
 with tab3 : 
     st.header("Composer votre portefeuille")
