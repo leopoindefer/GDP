86538709b6812167c350dd515048a42301715e5e
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-07 15:40:24 +0100

    Add files via upload

--- fonctions/asset_database.py
+++ fonctions/asset_database.py
@@ -12,34 +12,6 @@ class Library:
         df_indice = pd.read_csv(file_indice, delimiter=";")
         assets_indice = df_indice['ticker'].tolist()
         return assets_indice
-
-    def get_assets_all(self):
-        symbol_txt = []
-        symbol_nom = []
-        dict_symb = {}
-        for ind in self._indice:
-            file_path = f"data/indices/{ind}.csv"
-            indices_df = pd.read_csv(file_path, delimiter=";")
-            symbols_list = indices_df["ticker"].tolist()
-            symbols_name = indices_df["nom"].tolist()
-            symbol_txt.extend(symbols_list)
-            symbol_nom.extend(symbols_name)
-            dict_symb[ind] = {"tickers": symbols_list, "noms": symbols_name}
-        return symbol_txt
-
-    def get_dataframes(self):
-        def load_data(symbol):
-            try:
-                data = pd.read_csv(f"data/actions/{symbol}.csv")
-                data = data.set_index("Date").filter(like='Close')
-                return data
-            except Exception as e:
-                print(f"Error loading data for {symbol}: {e}")
-                return None
-
-        dataframes = {symbol: load_data(symbol) for symbol in self._selected_assets}
-        selected_dataframes = {symbol: df for symbol, df in dataframes.items() if df is not None}
-        return selected_dataframes
     
     def get_weights(self):
         file_poids = f"data/poids/{self._len_asset}.csv"
@@ -63,4 +35,24 @@ class Library:
             valeur = row[1]  # Première colonne comme valeur
             dict_assets_names[cle] = valeur
 
-        return dict_assets_names
+        return dict_assets_names.values(), dict_assets_names
+    
+    def get_assets_all(self):
+        dict_assets_names = self.get_assets_names()
+        list_assets = [cle for cle, valeur in dict_assets_names.items() if valeur == self._selected_assets]
+        return list_assets
+    
+    def get_dataframes(self):
+        list_assets = self.get_assets_all()
+        def load_data(symbol):
+            try:
+                data = pd.read_csv(f"data/actions/{symbol}.csv")
+                data = data.set_index("Date").filter(like='Close')
+                return data
+            except Exception as e:
+                print(f"Error loading data for {symbol}: {e}")
+                return None
+
+        dataframes = {symbol: load_data(symbol) for symbol in list_assets}
+        selected_dataframes = {symbol: df for symbol, df in dataframes.items() if df is not None}
+        return selected_dataframes
