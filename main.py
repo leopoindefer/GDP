13b6a01b129f1a8372501b1fe747b08d45502d50
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-08 10:53:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -124,8 +124,10 @@ with tab3 :
     list_assets = []
     for actions in portefeuille:
         [list_assets.append(cle) for cle, valeur in dict_assets_names.items() if valeur == actions]
-    list_assets = sum(list_assets, [])
-    st.write(list_assets)
+    flat_list_assets = []
+    for item in list_assets:
+        flat_list_assets += item
+    st.write(flat_list_assets)
     nb_acts = len(portefeuille)
     calcul = st.button('Calculer')
     if calcul:
