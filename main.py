79cff99e32222ba1bfd72088e7de5d31198fc30b
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-12 12:48:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -43,14 +43,13 @@ with tab1 :
     if periode == "1 mois":
         tableau, max_date = Analyse(dataframes).KPI_1month()
     elif periode == "6 mois":
-        tableau = Analyse(dataframes).KPI_6month()
+        tableau = Analyse(dataframes, dict_assets_names).KPI_6month()
     elif periode == "1 an":
         tableau = Analyse(dataframes).KPI_1year()
     elif periode == "5 ans":
         tableau = Analyse(dataframes).KPI_5year()
     st.dataframe(tableau.style.applymap(lambda x: 'color: red' if any('-' in words for words in x.split()) else 'color: green',subset = ['VAR']), column_config={"VISION": st.column_config.LineChartColumn(
         "VISION", y_min=0, y_max=200)})
-    st.write(max_date)
 
     col1_info, col2_info = st.columns((1, 8))
     with col1_info:
