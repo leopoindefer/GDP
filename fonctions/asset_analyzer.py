c6f292ec99369a57b4a9d66d1f5def7591c14803
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-12 11:56:56 +0100

    Add files via upload

--- fonctions/asset_analyzer.py
+++ fonctions/asset_analyzer.py
@@ -29,6 +29,7 @@ class Analyse(Transform):
                 #[cle for cle, valeur in dict_assets_names.items() if valeur == actions]
                 line = [str(val) for val in s_mois_prec[close_columns].values.flatten()]
                 name = [cle for cle, valeur in self._dict_assets_names.items() if valeur == symbol]
+                name = name.astype(str)
                 
                 liste_cours.append({
                     "SYMBOLE": symbol,
