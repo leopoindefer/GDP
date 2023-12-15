03bf505f666c04987dbcc075e7ad4c10d77e7d9c
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2023-12-15 12:30:59 +0100

    Add files via upload

--- fonctions/asset_projection.py
+++ fonctions/asset_projection.py
@@ -59,7 +59,8 @@ class Projection():
         tRent_upper = (div+gap_value_upper)/montant*100
         return tRend, tRent, tRent_lower, tRent_upper
 
-    def get_color(tRent):
+    def get_color(self):
+        tRend, tRent, tRent_lower, tRent_upper = self.get_rentability()
         return '#FF0000;' if tRent < 0 else '#008000;'
     
     def unit_projection(self):
