295b09c13c6f003bb59e8b2e151905c2d03fa5e3
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2024-01-04 16:11:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -109,10 +109,10 @@ with tab2:
             duree = st.date_input("Jusqu'à quand ?", pd.to_datetime(forecast["date"].iloc[-1]), min_value=pd.to_datetime(df_prophet["ds"].iloc[0]), max_value=pd.to_datetime(forecast["date"].iloc[-1]))
         try :
             nb_part, tx_rendement, rendement, tx_rentabilite, rentabilite, tx_renta_lower, renta_lower, tx_renta_upper, renta_upper = Projection(montant, duree, asset, df_prophet, forecast).unit_projection()
-            st.write(f'Nombre d action acheté : {nb_part}  
-            Taux de rendement de : {tx_rendement}, Rendement de {rendement}  
-            Taux de Rentabilité de : {tx_rentabilite}, Rentabilité de {rentabilite}  
-            Intervalle de confiance de rentabilité : [{renta_lower} : {renta_upper}]')
+            st.write(f'Nombre d action acheté : {nb_part}')
+            st.write(f'Taux de rendement de : {tx_rendement}, Rendement de {rendement}')
+            st.write(f'Taux de Rentabilité de : {tx_rentabilite}, Rentabilité de {rentabilite}')  
+            st.write(f'Intervalle de confiance de rentabilité : [{renta_lower} : {renta_upper}]')
         except:
             st.error("Pas de projection disponible")
 
