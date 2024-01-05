677522e3552bcc2d835e5d691c94122f457f7471
Léo Poindefer <75809650+leopoindefer@users.noreply.github.com>
2024-01-05 10:16:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -129,8 +129,10 @@ with tab3 :
             nb_acts = len(portefeuille)
              
             try:
-
-                if nb_acts <= 4:
+                
+                if nb_acts == 1:
+                    st.write('Merci de choisir plus d une action')
+                elif nb_acts <= 4 and nb_acts > 1:
                     list_assets = Library(None,liste_indice,portefeuille).get_symbol()
                     portfolio_dataframes = Library(None, None, list_assets).get_dataframes()
                     merged_df = Optimize(list_assets, nb_acts, portfolio_dataframes).process_data()
